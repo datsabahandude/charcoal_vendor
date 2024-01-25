7f7cfee2947db65c9f74e2061263d67a40fe9be0
datsabahandude <mohd.noor.amlee@gmail.com>
2024-01-25 17:40:17 +0800

    setup firebase #3

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:charcoal_vendor/screens/homepage.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
