bb4c799c9ce42739d833234fd24b9c69a522ee2d
datsabahandude <mohd.noor.amlee@gmail.com>
2024-01-30 20:08:13 +0800

    [progress] stock ui #2

--- lib/screens/stockpage.dart
+++ lib/screens/stockpage.dart
@@ -1,6 +1,9 @@
 import 'package:charcoal_vendor/screens/homepage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 
 import '../components/customappbar.dart';
 
@@ -12,11 +15,24 @@ class StockPage extends StatefulWidget {
 }
 
 class _StockPageState extends State<StockPage> {
+  String date = DateFormat('d MMM yyyy HH:mm').format(DateTime.now());
+  int? stock;
+  User? user = FirebaseAuth.instance.currentUser;
+  FirebaseFirestore fire = FirebaseFirestore.instance;
+  @override
+  void initState() {
+    super.initState();
+    // fetch amount from db
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(
           title: 'Stock', onPressed: () => Get.off(const HomePage())),
+      body: Center(
+        child: Column(children: <Widget>[]),
+      ),
     );
   }
 }
